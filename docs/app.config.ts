878fc64b6cbbdf2c9b96c9b8d6e2a19783a1087e
jer3m01 <jer3m01@jer3m01.com>
2025-08-05 20:10:58 +0200

    chore: first release

--- docs/app.config.ts
+++ docs/app.config.ts
@@ -1,39 +1,41 @@
-import { defineConfig } from "@solidjs/start/config";
 import { withSolidBase } from "@kobalte/solidbase/config";
+import { defineConfig } from "@solidjs/start/config";
 
-export default defineConfig(withSolidBase(
-  // SolidStart config
-  {
-    server: {
-      prerender: {
-        crawlLinks: true
-      }
-    }
-  },
-  // SolidBase config
-  {
-    title: "@kobalte/themes",
-    titleTemplate: ":title - @kobalte/themes",
-    description: "Theme utilities for Solid & SolidStart",
-    themeConfig: {
-      sidebar: {
-        "/": [
-          {
-            title: "Overview",
-            collapsed: false,
-            items: [
-              {
-                title: "Home",
-                link: "/"
-              },
-              {
-                title: "About",
-                link: "/about"
-              }
-            ]
-          }
-        ]
-      }
-    }
-  }
-));
+export default defineConfig(
+	withSolidBase(
+		// SolidStart config
+		{
+			server: {
+				prerender: {
+					crawlLinks: true,
+				},
+			},
+		},
+		// SolidBase config
+		{
+			title: "@kobalte/themes",
+			titleTemplate: ":title - @kobalte/themes",
+			description: "Theme utilities for Solid & SolidStart",
+			themeConfig: {
+				sidebar: {
+					"/": [
+						{
+							title: "Overview",
+							collapsed: false,
+							items: [
+								{
+									title: "Home",
+									link: "/",
+								},
+								{
+									title: "About",
+									link: "/about",
+								},
+							],
+						},
+					],
+				},
+			},
+		},
+	),
+);
--- docs/src/entry-server.tsx
+++ docs/src/entry-server.tsx
@@ -1,22 +1,22 @@
+import { getHtmlProps } from "@kobalte/solidbase/server";
 // @refresh reload
-import { createHandler, StartServer } from "@solidjs/start/server";
-import {getHtmlProps} from "@kobalte/solidbase/server";
+import { StartServer, createHandler } from "@solidjs/start/server";
 
 export default createHandler(() => (
-  <StartServer
-    document={({ assets, children, scripts }) => (
-      <html {...getHtmlProps()}>
-        <head>
-          <meta charset="utf-8" />
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <link rel="icon" href="/favicon.ico" />
-          {assets}
-        </head>
-        <body>
-          <div id="app">{children}</div>
-          {scripts}
-        </body>
-      </html>
-    )}
-  />
+	<StartServer
+		document={({ assets, children, scripts }) => (
+			<html {...getHtmlProps()}>
+				<head>
+					<meta charset="utf-8" />
+					<meta name="viewport" content="width=device-width, initial-scale=1" />
+					<link rel="icon" href="/favicon.ico" />
+					{assets}
+				</head>
+				<body>
+					<div id="app">{children}</div>
+					{scripts}
+				</body>
+			</html>
+		)}
+	/>
 ));
